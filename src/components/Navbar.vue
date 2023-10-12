b2ca6676d2c82af714fd93c7af38a657197bd650
Mateus Maranhão <feradourada@gmail.com>
2023-10-12 11:12:34 -0300

    Start project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Navbar</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+    components: {
+
+    },
+})
+export default class Navbar extends Vue { }
+</script>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,16 @@
+<template>
+    <navbar></navbar>
+    <h1>Portfolio</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Navbar from '@/components/Navbar.vue'
+
+@Options({
+    components: {
+        Navbar
+    },
+})
+export default class Portfolio extends Vue { }
+</script>
