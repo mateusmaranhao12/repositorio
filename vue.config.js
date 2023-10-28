35a0f17715c9d0f5fdb166ec36b8b029b4d9b048
Mateus Maranhão <feradourada@gmail.com>
2023-10-28 20:56:59 -0300

    Update portfolio

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Novo Título da Página';
+      return args;
+    });
+  },
+};
